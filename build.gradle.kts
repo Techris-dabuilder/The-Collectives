8832fa385e942f8ba6ed2bbd3cd545030cd13722
Techris-dabuilder <tuyishimee.chris@gmail.com>
2026-08-14 00:12:45 +0200

    Refactor build.gradle.kts to include repositories
    Removed comments and added allprojects block for repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
-// Root build.gradle.kts
-// Location: /build.gradle.kts (top level of the repo)
-
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
