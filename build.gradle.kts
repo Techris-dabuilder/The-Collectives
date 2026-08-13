dc1b4e0cb34cf3be0f35d28dd05ab59f68699e9e
Techris-dabuilder <tuyishimee.chris@gmail.com>
2026-08-13 21:30:19 +0200

    Add initial build.gradle.kts with plugins

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Root build.gradle.kts
+// Location: /build.gradle.kts (top level of the repo)
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
